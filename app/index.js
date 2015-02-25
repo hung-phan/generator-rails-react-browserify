4adeb47b5fb0a6910184e7369c935cb56c89df37
hung-phan <colorvisavn@gmail.com>
2015-02-25 10:23:14 +0700

    Update react router

--- app/index.js
+++ app/index.js
@@ -220,7 +220,7 @@ module.exports = yeoman.generators.Base.extend({
                         "//= require react\n" +
                         "//= require react_ujs\n" +
                         (this.includeSuperagent ? "//= require superagent/superagent\n" : "") +
-                        "//= require react-router/dist/react-router\n" +
+                        "//= require react-router/build/global/ReactRouter\n" +
                         "//= require build/main-build");
 
 
