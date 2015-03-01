f4e8e4f6588b6bd7097b05e817265aac05e2469b
hung-phan <colorvisavn@gmail.com>
2015-03-02 02:38:42 +0700

    Remove dummy config

--- app/index.js
+++ app/index.js
@@ -218,10 +218,8 @@ module.exports = yeoman.generators.Base.extend({
     file = file.replace("//= require turbolinks\n", '')
                .replace("//= require_tree .",
                         "//= require react\n" +
-                        "//= require react_ujs\n" +
-                        (this.includeSuperagent ? "//= require superagent/superagent\n" : "") +
-                        "//= require react-router/build/global/ReactRouter\n" +
-                        "//= require build/main.bundle");
+                        "//= require build/main.bundle" +
+                        "//= require react_ujs\n");
 
 
     this.write(path, file);
